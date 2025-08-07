f6fdbeb80c793432c96703a463081a6ac78e7f53
Shan Krish <shankrish1001@gmail.com>
2025-08-07 14:02:14 +0530

    remove gradle

--- src/main/java/com/shankrish/crudpostgresemployee/controller/EmployeeController.java
+++ src/main/java/com/shankrish/crudpostgresemployee/controller/EmployeeController.java
@@ -1,4 +1,45 @@
 package com.shankrish.crudpostgresemployee.controller;
 
+import com.shankrish.crudpostgresemployee.model.Employee;
+import com.shankrish.crudpostgresemployee.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employee")
 public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployee(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        return employeeService.updateEmployee(id, employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteEmployee(@PathVariable Long id) {
+        return employeeService.deleteEmployee(id);
+    }
+
 }
